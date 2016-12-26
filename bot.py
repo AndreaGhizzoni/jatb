d7ae09cc783a6cd6345fc3c933c8a30b158debd4
AndreaGhizzoni <andrea.ghz@gmail.com>
2016-12-26 18:53:19 +0100

    fixing while learning

--- bot.py
+++ bot.py
@@ -6,28 +6,28 @@ from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.namedtuple import InlineQueryResultArticle, InputTextMessageContent 
 
 
-def echo(msg):
+def do_nothing(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
+
+def echo(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
     if content_type == 'text':
         bot.sendMessage(chat_id, msg['text'])
 
 
-
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text='Press me', callback_data='press')],
     ])
-
     bot.sendMessage(chat_id, 'Use inline keyboard', reply_markup=keyboard)
 
 def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     print('Callback Query:', query_id, from_id, query_data)
-
     bot.answerCallbackQuery(query_id, text='Got it')
 
 
@@ -39,7 +39,7 @@ def on_inline_query(msg):
 
         articles = [InlineQueryResultArticle(
             id='abc',
-            title=query_string,
+            title= (query_string if '' else 'empty query'),
             input_message_content=InputTextMessageContent(
                 message_text='Hello'
             )
@@ -52,9 +52,11 @@ def on_chosen_inline_result(msg):
     print ('Chosen Inline Result:', result_id, from_id, query_string)
 
 
+
 bot = telepot.Bot(rtoken.getToken())
 answerer = telepot.helper.Answerer(bot)
-bot.message_loop({'inline_query': on_inline_query,
+bot.message_loop({'chat':do_nothing,
+                  'inline_query': on_inline_query,
                   'chosen_inline_result': on_chosen_inline_result},
                   run_forever='Listening ...')
 
